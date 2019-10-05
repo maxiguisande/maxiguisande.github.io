fb32ee6330306328d54f62c842f50a15ed9578f9
Maximiliano Guisande <maxipg@gmail.com>
2019-10-05 15:56:36 -0300

    init3

--- sw.js
+++ sw.js
@@ -48,7 +48,9 @@ self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.filter(function(cacheName) {}).map(function(cacheName) {
+                cacheNames.filter(function(cacheName) {
+                  return true;
+                }).map(function(cacheName) {
                     return caches.delete(cacheName);
                 })
             );
