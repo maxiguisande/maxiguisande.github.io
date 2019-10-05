8a602ae649a3707c21d472b31ac987ef74b68937
Maximiliano Guisande <maxipg@gmail.com>
2019-10-05 16:55:11 -0300

    init3

--- sw.js
+++ sw.js
@@ -41,11 +41,16 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          return caches.delete(cacheName);
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
       );
     })
