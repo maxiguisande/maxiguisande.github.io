4058297a2d83011d8380f866983f7e688053fcd3
Maximiliano Guisande <maxipg@gmail.com>
2019-10-05 18:00:53 -0300

    init3

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHENAME = 'TP2Grupo1-v4';
+const CACHENAME = "TP2Grupo1-v4";
 
 const FILES = [
-  'assets/img/Hernan.jpg',
-  'assets/img/Maxi.jpg',
-  'assets/img/Julian.jpg',
-  'assets/img/indoor.svg',
-  'assets/img/pwa.png',
-  'assets/icon.woff2',
-  'css/estilos.css',
-  'css/materialize.css',
-  'css/materialize.min.css',
-  'js/init.js',
-  'js/materialize.js',
-  'js/materialize.min.js',
-  'index.html',
-  'pages/trabajos.html',
-  'assets/icons/512.png',
-  'assets/icons/192.png'
+  "assets/img/Hernan.jpg",
+  "assets/img/Maxi.jpg",
+  "assets/img/Julian.jpg",
+  "assets/img/indoor.svg",
+  "assets/img/pwa.png",
+  "assets/icon.woff2",
+  "css/estilos.css",
+  "css/materialize.css",
+  "css/materialize.min.css",
+  "js/init.js",
+  "js/materialize.js",
+  "js/materialize.min.js",
+  "index.html",
+  "pages/trabajos.html",
+  "assets/icons/512.png",
+  "assets/icons/192.png"
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHENAME).then(function(cache) {
       return cache.addAll(FILES);
@@ -27,20 +27,6 @@ self.addEventListener('install', function(event) {
   );
 });
 
-/*
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-*/
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
@@ -49,18 +35,18 @@ self.addEventListener("fetch", function(event) {
   );
 });
 
-self.addEventListener('activate', function(event) {
-  var version = 'v4';
+self.addEventListener("activate", function(event) {
+  var version = "v4";
   event.waitUntil(
     caches.keys()
     .then(cacheNames =>
       Promise.all(
         cacheNames
-        .map(c => c.split('-'))
-        .filter(c => c[0] === 'cachestore')
+        .map(c => c.split("-"))
+        .filter(c => c[0] === "cachestore")
         .filter(c => c[1] !== version)
         .map(
-          c => caches.delete(c.join('-'))
+          c => caches.delete(c.join("-"))
         )
       )
     )
